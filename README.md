09dfa1cd64102ecb94be9dc6d7230dee7eebd423
JakubBlaha <jakub.blaha@gmail.com>
2018-05-14 16:17:47 +0200

    V 1.1

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FastFastLauncher
+
+## V 1.1
+### New features:
+ - You can now open file by hitting the *Enter* key after typing search expression
+ - Window now stays always on top of other windows
+ - Updated *dropdown* showing logic
+ - Fixed few bugs
+
+## V 1.0
+*Initial release*
+### Supports:
+ - Desktop files indexing
+ - Search tool
+ - File opening by clicking 
+
+*Please note that this version does not support opening files with Enter key hit*
--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from win32api import GetSystemMetrics
 from win32gui import GetCursorInfo
 from _thread import start_new
 from time import sleep
+from nop import NOP
 import os
 import sys
 import win32com.client
+import win32gui
+import win32con
+import string
 
 import kivy
 kivy.require('1.10.0')
@@ -16,6 +20,7 @@ Config.set('graphics', 'width', '300')
 Config.set('graphics', 'borderless', '1')
 Config.set('graphics', 'minimum_height', '40')
 Config.set('graphics', 'minimum_width', '300')
+Config.set('graphics', 'resizable', 0)
 
 from kivy.app import App
 from kivy.core.window import Window
@@ -29,11 +34,79 @@ from kivy.uix.recycleview import RecycleView
 from kivymd.button import MaterialIconButton
 
 
-class RV(RecycleView):
-    def __init__(self, **kwargs):
-        super(RV, self).__init__(**kwargs)
+def get_desktop_dir() -> list:
+    desktop_path = os.path.join(os.environ['HOMEPATH'], 'Desktop')
+    
+    return desktop_path
 
-        self.data = [{'text': 'spam'} for x in range(100)]
+def get_lnk_target(lnk: str) -> str:
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortCut(lnk)
+    return shortcut.Targetpath
+
+def width(value: int) -> tuple:
+    h = Window.height
+    size = (value, h)
+    
+    return size
+
+def height(value: int) -> tuple:
+    w = Window.width
+    size = (w, value)
+
+    return size
+
+def prepare_rv_list(paths: list) -> list:
+    LNK = '.lnk'
+    paths = [
+        get_lnk_target(path)
+        if os.path.splitext(path)[1] == LNK else path for path in paths
+    ]
+
+    basenames = [os.path.splitext(os.path.basename(path))[0] for path in paths]
+    exts = [os.path.splitext(path)[1] for path in paths]
+
+    items = [{
+        'path': path,
+        'text': base,
+        'ext': ext
+        } for path, base, ext in zip(paths, basenames, exts)
+        ]
+
+    return items
+
+def get_filtered(string_: str) -> list:
+    paths = list_desktop()
+    items = prepare_rv_list(paths)
+
+    condition = lambda item: string_.lower() in f"{item['text']}{item['ext']}".lower()
+    filtered_items = filter(condition, items)
+
+    return filtered_items
+
+def list_desktop() -> list:
+    desktop = get_desktop_dir()
+    items = os.listdir(get_desktop_dir())
+    items.remove('desktop.ini')
+    for item, index in zip(items, range(len(items))):
+        items[index] = f"{desktop}\\{item}"
+
+    return items
+
+def open_file(path: str):
+    path = os.path.abspath(path)
+    start = lambda path: os.startfile(path)
+
+    if os.path.exists(path):
+        start(path)
+
+    else:
+        x86 = 'Program Files (x86)'
+        x64 = 'Program Files'
+
+        path = path.replace(x86, x64)
+
+        start(path)
 
 
 class CustBoxLayout(BoxLayout):
@@ -43,71 +116,79 @@ class CustBoxLayout(BoxLayout):
         self.dropdown_shown = False
 
     def dropdown_show(self):
-        self.dropdown_shown = True
+        if self.dropdown_shown:
+            return
 
-        curr_width = Window.width
-        anim = Animation(
-            size=(curr_width, 300), transition=AnimationTransition.out_expo)
-        anim.start(Window)
+        else:
+            self.dropdown_shown = True
 
-        orig_h = self.ids.dropdown_button.height
+        TRANS = 'out_expo'
+        btn = self.ids.dropdown_button
+        
+        btn.on_release = NOP
 
-        btn_anim_dur_half = .2
+        win_anim = Animation(size=height(300), transition=TRANS)
+        button_out_anim = Animation(height=120, transition=TRANS, duration=.2)
+        button_in_anim = Animation(height=btn.height, transition=TRANS, duration=.2)
 
-        icon_anim = Animation(
-            height=120,
-            duration=btn_anim_dur_half,
-            transition=AnimationTransition.out_expo)
-        icon_anim.start(self.ids.dropdown_button)
+        def update_button(*args):
+            btn.icon = 'md-cancel'
+            btn.on_release = self.dropdown_hide
 
-        def after_btn_anim(*args):
-            self.ids.dropdown_button.icon = 'md-cancel'
+        button_out_anim.on_complete = update_button
+        
+        start_times = {
+        # ~ Animation: [after, widget] ~
+            win_anim: [0, Window],
+            button_out_anim: [0, btn],
+            button_in_anim: [.2, btn]
+        }
 
-            icon_anim = Animation(
-                height=orig_h,
-                duration=btn_anim_dur_half,
-                transition=AnimationTransition.out_expo)
-            icon_anim.start(self.ids.dropdown_button)
+        for anim, values in start_times.items():
+            time = values[0]
+            widget = values[1]
 
-            self.ids.dropdown_button.on_release = self.dropdown_hide
+            fn = lambda clock, anim=anim, widget=widget: anim.start(widget)
 
-        Clock.schedule_once(after_btn_anim, btn_anim_dur_half)
+            Clock.schedule_once(fn, time)
 
-        Window.on_cursor_leave = self.dropdown_hide
 
     def dropdown_hide(self):
         if not self.dropdown_shown:
             return
 
-        self.dropdown_shown = False
+        else:
+            self.dropdown_shown = False
 
-        curr_width = Window.width
-        anim = Animation(
-            size=(curr_width, 32), transition=AnimationTransition.out_expo)
-        anim.start(Window)
+        TRANS = 'out_expo'
+        btn = self.ids.dropdown_button
+        
+        btn.on_release = NOP
 
-        orig_h = self.ids.dropdown_button.height
+        win_anim = Animation(size=height(32), transition=TRANS)
+        button_out_anim = Animation(height=120, transition=TRANS, duration=.2)
+        button_in_anim = Animation(height=btn.height, transition=TRANS, duration=.2)
 
-        btn_anim_dur_half = .2
+        def update_button(*args):
+            btn.icon = 'md-arrow-drop-down-circle'
+            btn.on_release = self.dropdown_show
 
-        icon_anim = Animation(
-            height=120,
-            duration=btn_anim_dur_half,
-            transition=AnimationTransition.out_expo)
-        icon_anim.start(self.ids.dropdown_button)
+        button_out_anim.on_complete = update_button
+        
+        start_times = {
+        # ~ Animation: [after, widget] ~
+            win_anim: [0, Window],
+            button_out_anim: [0, btn],
+            button_in_anim: [.2, btn]
+        }
 
-        def after_btn_anim(*args):
-            self.ids.dropdown_button.icon = 'md-arrow-drop-down-circle'
+        for anim, values in start_times.items():
+            time = values[0]
+            widget = values[1]
 
-            icon_anim = Animation(
-                height=orig_h,
-                duration=btn_anim_dur_half,
-                transition=AnimationTransition.out_expo)
-            icon_anim.start(self.ids.dropdown_button)
+            fn = lambda clock, anim=anim, widget=widget: anim.start(widget)
 
-            self.ids.dropdown_button.on_release = self.dropdown_show
-
-        Clock.schedule_once(after_btn_anim, btn_anim_dur_half)
+            Clock.schedule_once(fn, time)
 
 
 class LauncherApp(App):
@@ -119,34 +200,30 @@ class LauncherApp(App):
     def build(self):
         self.l = CustBoxLayout()
 
-        self.load_list_items()
+        self.load_desktop()
 
         inp = self.l.ids.search_field
-
         inp.bind(text=self.filter_results)
+        
+        TITLE = 'FastFastLauncher'
+        self.title = TITLE
 
         return self.l
 
-    def load_list_items(self):
-        paths = self.list_desktop()
-        items = self.prepare_rv_list(paths)
+    def load_desktop(self):
+        paths = list_desktop()
+        items = prepare_rv_list(paths)
 
         self.l.ids.item_list.data = items
-
-    def get_lnk_target(self, lnk):
-        shell = win32com.client.Dispatch("WScript.Shell")
-        shortcut = shell.CreateShortCut(lnk)
-        return shortcut.Targetpath
 
     def enter_countdown(self):
         Window.orig_on_touch_down = Window.on_touch_down
 
         Window.on_touch_down = self.cancel_countdown
-        if hasattr(Window, 'on_cursor_enter'):
-            del Window.on_cursor_enter
 
+        TIME = .5
         self.countdown_clock = Clock.schedule_once(self.check_countdown_valid,
-                                                   1)
+                                                   TIME)
 
     def cancel_countdown(self, touch):
         self.countdown_clock.cancel()
@@ -217,81 +294,107 @@ class LauncherApp(App):
 
         anim.start(Window)
 
-    def get_desktop_dir(self):
-        return os.path.join(os.environ["HOMEPATH"], "Desktop")
+    def item_click_callback(self, path):
+        open_file(path)
+        self.l.dropdown_hide()
 
-    def list_desktop(self):
-        desktop = self.get_desktop_dir()
-        items = os.listdir(self.get_desktop_dir())
-        items.remove('desktop.ini')
-        for item, index in zip(items, range(len(items))):
-            items[index] = f"{desktop}\\{item}"
+    def enter_hit_callback(self):
+        inp = self.l.ids.search_field
+        inp.text = ''
 
-        return items
+        RV = self.l.ids.item_list
+        data = RV.data
 
-    def start_file(self, path):
-        try:
-            os.startfile(os.path.abspath(path))
-        except FileNotFoundError:
-            path = path.replace('Program Files (x86)', 'Program Files')
-            os.startfile(path)
+        if len(data) == 0:
+            return
 
+        first_item = data[0]
+        path = first_item['path']
+
+        open_file(path)
         self.l.dropdown_hide()
 
     def filter_results(self, *args):
-        inp = self.l.ids.search_field
-
-        text = inp.text
+        text = args[1]
 
         if text.strip() == '':
-            self.cancel_filter()
+            self.l.dropdown_hide()
             return
 
-        filtered_items = self.get_filtered(text)
+        else:
+            self.l.dropdown_show()
+
+        filtered_items = get_filtered(text)
 
         RV = self.l.ids.item_list
         RV.data = filtered_items
 
-    def prepare_rv_list(self, paths):
-        LNK = '.lnk'
-        paths = [
-            self.get_lnk_target(path)
-            if os.path.splitext(path)[1] == LNK else path for path in paths
-        ]
+    def find_hwnd(self):
+        Window.create_window()
 
-        basenames = [os.path.splitext(os.path.basename(path))[0] for path in paths]
-        exts = [os.path.splitext(path)[1] for path in paths]
+        TITLE = self.title
+        self.HWND = win32gui.FindWindow(None, TITLE)
+        
+        return self.HWND
 
-        items = [{
-            'path': path,
-            'text': base,
-            'ext': ext
-            } for path, base, ext in zip(paths, basenames, exts)
-            ]
+    def pos(self, pos: tuple=(None, None), size: tuple=(None, None)):
+        DEFAULT = (None, None)
 
-        return items
+        if pos == DEFAULT and size == DEFAULT:
+            raise Exception('You have to put in atleast one of two parameters')
 
-    def get_filtered(self, string):
-        paths = self.list_desktop()
-        items = self.prepare_rv_list(paths)
-        filtered_items = [
-            item for item in items if string.lower().strip() in (item['text'] + item['ext']).lower()
-        ]
+        if pos == DEFAULT:
+            x = Window.left
+            y = Window.top
+            
+            pos = (x, y)
 
-        return filtered_items
+        if size == DEFAULT:
+            w = Window.width
+            h = Window.height
 
-    def cancel_filter(self):
-        self.load_list_items()
+            size = (w, h)
+
+        hwnd = self.HWND
+        win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, *pos, *size, 0)
+
+    def set_always_on_top(self):
+        if not hasattr(self, 'HWND'):
+            raise Exception('Call "find_hwnd" method first!')
+
+        hwnd = self.HWND
+        rect = win32gui.GetWindowRect(hwnd)
+        x = rect[0]
+        y = rect[1]
+        w = rect[2] - x
+        h = rect[3] - y
+
+        win32gui.SetWindowPos(self.HWND, win32con.HWND_TOPMOST, x, y, w, h, 0)
+
+    def on_start(self):
+        # Executed when window loaded so it can set the position
+        self.find_hwnd()
+
+        screen_width = GetSystemMetrics(0)
+        window_width = Window.width
+
+        x = (screen_width // 2) - (window_width // 2)
+        y = 0
+
+        pos = ((x, y))
+        self.pos(pos)
+
+        def on_draw():
+            Window.old_on_draw()
+            self.set_always_on_top()
+        
+        Window.old_on_draw = Window.on_draw
+        Window.on_draw = on_draw
 
 
 if __name__ == '__main__' or __name__ == 'main':
-    screen_width = GetSystemMetrics(0)
-    window_width = Window.width
-    pos = (screen_width // 2) - (window_width // 2)
-
-    Window.left = pos
-    Window.top = 0
-    Window.on_hide = Window.raise_window
-
     launcher = LauncherApp()
     launcher.run()
+
+# TODO
+# Add support for moving the window
